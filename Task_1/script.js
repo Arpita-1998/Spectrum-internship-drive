a2263fb5d61860f60ec105b3396fb17c98cb8e54
Arpita <40335166+Arpita-1998@users.noreply.github.com>
2020-05-17 02:10:15 +0530

    Javascript file

--- Task_1/script.js
+++ Task_1/script.js
@@ -1,7 +1,20 @@
+function showTime(){
+
 var today=new Date();
-var date=today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear()
+
+var date=today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear()
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = 'Time:' +' ' +time +'.' +'Date:' +' ' +date;
+
+const AmPm = today.getHours() >= 12 ? 'PM' : 'AM';
+
+var dateTime = 'Time:' +' ' +time +' ' +AmPm +'.' +'Date:' +' ' +date;
 var newdateTime = dateTime.replace(/\./g, "<br/>"); /* replace . with <br> */
 
 document.getElementById('dateandtime').innerHTML = newdateTime;
+
+setTimeout(showTime, 1000); /* showTime() is called repeatedly after every 1000ms, i.e. 1sec */
+
+}
+
+showTime();
+
