0e7afed80f50d3c3c1e144090af191a9931507bb
Arpita <40335166+Arpita-1998@users.noreply.github.com>
2020-05-18 22:52:40 +0530

    Add files via upload

---
+++ Task_2/index.js
@@ -0,0 +1,6 @@
+$(window).load(function() {
+
+   /* $(".loader_inner").fadeOut() */
+	$(".loader").delay(5000).fadeOut(100); // gif will stay for 5 secs, fade out in 100ms
+})
+
---
+++ Task_2/script.js
@@ -0,0 +1,37 @@
+$(window).load(function() {
+
+   /* $(".loader_inner").fadeOut() */
+	$(".loader").delay(2000).fadeOut(100); // gif will stay for 2 secs, fade out in 100ms
+})
+
+})
+
+function showTime(){
+
+var today = new Date();
+
+var date = today.getDate();
+var month = today.getMonth();
+var year = today.getFullYear();
+var hour = today.getHours();
+var min = today.getMinutes();
+var secs = today.getSeconds();
+
+if (hour > 12) { hour = hour-12 ; }
+if (hour == 0) { hour = 12 ; }
+
+const AmPm = today.getHours() >= 12 ? 'PM' : 'AM';
+
+var dateTime = 'Time:' +' ' +hour +':' +min +':' +secs +' ' +AmPm +'.' +'Date:' +' ' +date +'/' +month +'/' +year;
+var newdateTime = dateTime.replace(/\./g, "<br/>"); /* replace . with <br> */
+
+document.getElementById('dateandtime').innerHTML = newdateTime;
+
+setTimeout(showTime, 1000); /* showTime() is called repeatedly after every 1000ms, i.e. 1sec */
+
+}
+
+showTime();
+
+
+
